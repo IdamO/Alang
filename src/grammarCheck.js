9a6fcfb4c36dfda717ba6bcc64dad522cad668f1
IdamO <emmanuel.obiahu@gmail.com>
2015-10-25 00:48:44 -0400

    Slight debugging (C to JS transmission)

--- src/grammarCheck.js
+++ src/grammarCheck.js
@@ -1,12 +1,16 @@
-var SERVER_IP = '10.121.251.137:8070';
+var SERVER_IP = '10.121.39.133:8070';
+
+console.log('loaded Javascript!');
 
 function fetchCorrections(text) {
+  var url = "http://" + SERVER_IP + "/api?sentence=" + encodeURIComponent(text);
+  console.log("About to request:",  url);
+  req.open('GET', url);
   var req = new XMLHttpRequest();
-  req.open('GET', "http://" + SERVER_IP + "/api/?sentence=" + text, true);
   req.onload = function(e) {
     if (req.readyState == 4) {
       if(req.status == 200) {
-        console.log(req.responseText);
+        console.log("got a response!", req.responseText);
         var response = JSON.parse(req.responseText);
       } else {
         console.log("Error", req.status);
@@ -18,9 +22,10 @@ function fetchCorrections(text) {
 
 Pebble.addEventListener('appmessage', function(e) {
   var text = e.payload[0xabbababe];
+  console.log('got an app message!');
   fetchCorrections(text);
 });
 
 Pebble.addEventListener('ready', function() {
-  console.log("ready!");
+  console.log("Ready!");
 });
